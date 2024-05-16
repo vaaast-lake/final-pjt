17072d20c32bf04b1b817ceaa61c3851b8eb44a9
soyeon <lilyjo128@naver.com>
2024-05-16 13:34:51 +0900

    0516

---
+++ vue-project/src/views/LogoutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="handleLogout">로그아웃</button>
+</template>
+
+<script>
+import { useCounterStore } from '@/stores/counter'
+
+export default {
+  setup() {
+    const counterStore = useCounterStore()
+
+    const handleLogout = () => {
+      counterStore.logout()
+    }
+
+    return { handleLogout }
+  }
+}
+</script>
